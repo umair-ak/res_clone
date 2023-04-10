470e28275014bd64e34dabc03c84ef625217f469
Umair <uak1911@gmail.com>
2023-04-10 07:58:02 +0530

    passing data from webpage to app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express()
-app.set(bodyParser.urlencoded({extended:true}))
+const data = require(__dirname + "/data.js");
+app.use(bodyParser.urlencoded({extended:true}))
 const ejs = require("ejs");
 app.set("view engine","ejs");
 app.use(express.static(__dirname + "/public"))
@@ -9,10 +10,14 @@ app.use(express.static(__dirname + "/public"))
 
 
 app.get("/", function(req, res){
-    // res.sendFile(__dirname + "/index.html");
     res.render("result");
 });
 
+app.post("/",function(req,res){
+
+    var roll = req.body.rollno;
+    console.log(roll);
+});
 
 
 app.listen(3000,function(){
